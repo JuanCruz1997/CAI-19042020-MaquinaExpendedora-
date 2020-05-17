18113502e238e0f381e1212a7fa9907ae585e489
JuanCruz1997 <62308931+JuanCruz1997@users.noreply.github.com>
2020-05-17 14:10:52 -0300

    Forms Expendedora
    Implementación de IngresoLata()

--- Máquina Expendedora/ExpendedoraForms/frmIngresoLata.Designer.cs
+++ Máquina Expendedora/ExpendedoraForms/frmIngresoLata.Designer.cs
@@ -31,18 +31,16 @@
             this.lblIngresar = new System.Windows.Forms.Label();
             this.lstLatasIngreso = new System.Windows.Forms.ListBox();
             this.btnApagarIngreso = new System.Windows.Forms.Button();
-            this.lblCodigoMarca = new System.Windows.Forms.Label();
-            this.cmbTipoCodigo = new System.Windows.Forms.ComboBox();
+            this.lblCodigo = new System.Windows.Forms.Label();
             this.lblVolumen = new System.Windows.Forms.Label();
             this.txtVolumen = new System.Windows.Forms.TextBox();
             this.lblPropiedades = new System.Windows.Forms.Label();
             this.lblPrecio = new System.Windows.Forms.Label();
             this.lblMarca = new System.Windows.Forms.Label();
             this.lblSabor = new System.Windows.Forms.Label();
-            this.lblCodigoSabor = new System.Windows.Forms.Label();
-            this.cmbTipoSabor = new System.Windows.Forms.ComboBox();
             this.btnIngresar = new System.Windows.Forms.Button();
             this.btnVolver = new System.Windows.Forms.Button();
+            this.cmbCodigo = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // lblIngresar
@@ -69,7 +67,7 @@
             // 
             // btnApagarIngreso
             // 
-            this.btnApagarIngreso.Location = new System.Drawing.Point(272, 439);
+            this.btnApagarIngreso.Location = new System.Drawing.Point(272, 396);
             this.btnApagarIngreso.Name = "btnApagarIngreso";
             this.btnApagarIngreso.Size = new System.Drawing.Size(75, 32);
             this.btnApagarIngreso.TabIndex = 8;
@@ -77,28 +75,19 @@
             this.btnApagarIngreso.UseVisualStyleBackColor = true;
             this.btnApagarIngreso.Click += new System.EventHandler(this.btnApagar_Click);
             // 
-            // lblCodigoMarca
+            // lblCodigo
             // 
-            this.lblCodigoMarca.AutoSize = true;
-            this.lblCodigoMarca.Location = new System.Drawing.Point(12, 256);
-            this.lblCodigoMarca.Name = "lblCodigoMarca";
-            this.lblCodigoMarca.Size = new System.Drawing.Size(186, 17);
-            this.lblCodigoMarca.TabIndex = 9;
-            this.lblCodigoMarca.Text = "Seleccione código de marca";
-            // 
-            // cmbTipoCodigo
-            // 
-            this.cmbTipoCodigo.FormattingEnabled = true;
-            this.cmbTipoCodigo.Location = new System.Drawing.Point(200, 256);
-            this.cmbTipoCodigo.Name = "cmbTipoCodigo";
-            this.cmbTipoCodigo.Size = new System.Drawing.Size(146, 24);
-            this.cmbTipoCodigo.TabIndex = 10;
-            this.cmbTipoCodigo.SelectedIndexChanged += new System.EventHandler(this.cmbTipoCodigo_SelectedIndexChanged);
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(12, 256);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(123, 17);
+            this.lblCodigo.TabIndex = 9;
+            this.lblCodigo.Text = "Seleccione código";
             // 
             // lblVolumen
             // 
             this.lblVolumen.AutoSize = true;
-            this.lblVolumen.Location = new System.Drawing.Point(12, 328);
+            this.lblVolumen.Location = new System.Drawing.Point(13, 294);
             this.lblVolumen.Name = "lblVolumen";
             this.lblVolumen.Size = new System.Drawing.Size(112, 17);
             this.lblVolumen.TabIndex = 11;
@@ -106,7 +95,7 @@
             // 
             // txtVolumen
             // 
-            this.txtVolumen.Location = new System.Drawing.Point(142, 328);
+            this.txtVolumen.Location = new System.Drawing.Point(141, 294);
             this.txtVolumen.Name = "txtVolumen";
             this.txtVolumen.Size = new System.Drawing.Size(205, 22);
             this.txtVolumen.TabIndex = 12;
@@ -114,7 +103,7 @@
             // lblPropiedades
             // 
             this.lblPropiedades.AutoSize = true;
-            this.lblPropiedades.Location = new System.Drawing.Point(9, 369);
+            this.lblPropiedades.Location = new System.Drawing.Point(9, 333);
             this.lblPropiedades.Name = "lblPropiedades";
             this.lblPropiedades.Size = new System.Drawing.Size(179, 17);
             this.lblPropiedades.TabIndex = 13;
@@ -123,7 +112,7 @@
             // lblPrecio
             // 
             this.lblPrecio.AutoSize = true;
-            this.lblPrecio.Location = new System.Drawing.Point(13, 399);
+            this.lblPrecio.Location = new System.Drawing.Point(13, 360);
             this.lblPrecio.Name = "lblPrecio";
             this.lblPrecio.Size = new System.Drawing.Size(52, 17);
             this.lblPrecio.TabIndex = 14;
@@ -133,7 +122,7 @@
             // lblMarca
             // 
             this.lblMarca.AutoSize = true;
-            this.lblMarca.Location = new System.Drawing.Point(105, 399);
+            this.lblMarca.Location = new System.Drawing.Point(96, 360);
             this.lblMarca.Name = "lblMarca";
             this.lblMarca.Size = new System.Drawing.Size(51, 17);
             this.lblMarca.TabIndex = 15;
@@ -142,68 +131,58 @@
             // lblSabor
             // 
             this.lblSabor.AutoSize = true;
-            this.lblSabor.Location = new System.Drawing.Point(231, 399);
+            this.lblSabor.Location = new System.Drawing.Point(222, 360);
             this.lblSabor.Name = "lblSabor";
             this.lblSabor.Size = new System.Drawing.Size(50, 17);
             this.lblSabor.TabIndex = 16;
             this.lblSabor.Text = "Sabor:";
             // 
-            // lblCodigoSabor
-            // 
-            this.lblCodigoSabor.AutoSize = true;
-            this.lblCodigoSabor.Location = new System.Drawing.Point(12, 293);
-            this.lblCodigoSabor.Name = "lblCodigoSabor";
-            this.lblCodigoSabor.Size = new System.Drawing.Size(183, 17);
-            this.lblCodigoSabor.TabIndex = 17;
-            this.lblCodigoSabor.Text = "Seleccione código de sabor";
-            this.lblCodigoSabor.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // cmbTipoSabor
-            // 
-            this.cmbTipoSabor.FormattingEnabled = true;
-            this.cmbTipoSabor.Location = new System.Drawing.Point(202, 293);
-            this.cmbTipoSabor.Name = "cmbTipoSabor";
-            this.cmbTipoSabor.Size = new System.Drawing.Size(144, 24);
-            this.cmbTipoSabor.TabIndex = 18;
-            this.cmbTipoSabor.SelectedIndexChanged += new System.EventHandler(this.cmbTipoSabor_SelectedIndexChanged);
-            // 
             // btnIngresar
             // 
             this.btnIngresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnIngresar.Location = new System.Drawing.Point(132, 439);
+            this.btnIngresar.Location = new System.Drawing.Point(141, 396);
             this.btnIngresar.Name = "btnIngresar";
             this.btnIngresar.Size = new System.Drawing.Size(85, 32);
             this.btnIngresar.TabIndex = 19;
             this.btnIngresar.Text = "Ingresar";
             this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
             // 
             // btnVolver
             // 
             this.btnVolver.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnVolver.Location = new System.Drawing.Point(12, 439);
+            this.btnVolver.Location = new System.Drawing.Point(12, 396);
             this.btnVolver.Name = "btnVolver";
             this.btnVolver.Size = new System.Drawing.Size(75, 32);
             this.btnVolver.TabIndex = 20;
             this.btnVolver.Text = "< Volver";
             this.btnVolver.UseVisualStyleBackColor = true;
+            this.btnVolver.Click += new System.EventHandler(this.btnVolver_Click);
+            // 
+            // cmbCodigo
+            // 
+            this.cmbCodigo.FormattingEnabled = true;
+            this.cmbCodigo.Location = new System.Drawing.Point(141, 256);
+            this.cmbCodigo.Name = "cmbCodigo";
+            this.cmbCodigo.Size = new System.Drawing.Size(205, 24);
+            this.cmbCodigo.TabIndex = 10;
+            this.cmbCodigo.SelectedIndexChanged += new System.EventHandler(this.cmbTipoCodigo_SelectedIndexChanged);
             // 
             // frmIngresoLata
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(359, 480);
+            this.ClientSize = new System.Drawing.Size(359, 440);
             this.Controls.Add(this.btnVolver);
             this.Controls.Add(this.btnIngresar);
-            this.Controls.Add(this.cmbTipoSabor);
-            this.Controls.Add(this.lblCodigoSabor);
             this.Controls.Add(this.lblSabor);
             this.Controls.Add(this.lblMarca);
             this.Controls.Add(this.lblPrecio);
             this.Controls.Add(this.lblPropiedades);
             this.Controls.Add(this.txtVolumen);
             this.Controls.Add(this.lblVolumen);
-            this.Controls.Add(this.cmbTipoCodigo);
-            this.Controls.Add(this.lblCodigoMarca);
+            this.Controls.Add(this.cmbCodigo);
+            this.Controls.Add(this.lblCodigo);
             this.Controls.Add(this.btnApagarIngreso);
             this.Controls.Add(this.lstLatasIngreso);
             this.Controls.Add(this.lblIngresar);
@@ -220,17 +199,15 @@
         private System.Windows.Forms.Label lblIngresar;
         private System.Windows.Forms.ListBox lstLatasIngreso;
         private System.Windows.Forms.Button btnApagarIngreso;
-        private System.Windows.Forms.Label lblCodigoMarca;
-        private System.Windows.Forms.ComboBox cmbTipoCodigo;
+        private System.Windows.Forms.Label lblCodigo;
         private System.Windows.Forms.Label lblVolumen;
         private System.Windows.Forms.TextBox txtVolumen;
         private System.Windows.Forms.Label lblPropiedades;
         private System.Windows.Forms.Label lblPrecio;
         private System.Windows.Forms.Label lblMarca;
         private System.Windows.Forms.Label lblSabor;
-        private System.Windows.Forms.Label lblCodigoSabor;
-        private System.Windows.Forms.ComboBox cmbTipoSabor;
         private System.Windows.Forms.Button btnIngresar;
         private System.Windows.Forms.Button btnVolver;
+        private System.Windows.Forms.ComboBox cmbCodigo;
     }
 }
--- Máquina Expendedora/ExpendedoraForms/frmIngresoLata.cs
+++ Máquina Expendedora/ExpendedoraForms/frmIngresoLata.cs
@@ -28,113 +28,117 @@ namespace ExpendedoraForms
             lstLatasIngreso.DataSource = null;
             lstLatasIngreso.DataSource = latas;
         }
-        private void CargarComboTipoCodigo()
+        private void CargarComboCodigo()
         {
             List<String> tipos = new List<string>();
-            tipos.Add("CO");
-            tipos.Add("SP");
-            tipos.Add("FA");
-            cmbTipoCodigo.DataSource = tipos;
+            tipos.Add("(Elija una opción)");
+            tipos.Add("CO1");
+            tipos.Add("CO2");
+            tipos.Add("SP1");
+            tipos.Add("SP2");
+            tipos.Add("FA1");
+            tipos.Add("FA2");
+            cmbCodigo.DataSource = tipos;
         }
-        private void CargarComboTipoSabor()
+        private void Cambiarlbl()
         {
-            List<String> tipos = new List<string>();
-            tipos.Add("1");
-            tipos.Add("2");
-            cmbTipoSabor.DataSource = tipos;
-        }
-        private void CambiarlblMarca()
-        {
-            if (cmbTipoCodigo.SelectedValue.ToString() == "CO")
+            if (cmbCodigo.SelectedValue.ToString() == "CO1")
             {
+                lblPrecio.Text = "Precio: $35";
                 lblMarca.Text = "Marca: Coca Cola";
-            }
-            else if (cmbTipoCodigo.SelectedValue.ToString() == "SP")
-            {
-                lblMarca.Text = "Marca: Sprite";
-            }
-            else if (cmbTipoCodigo.SelectedValue.ToString() == "FA")
-            {
-                lblMarca.Text = "Marca: Fanta";
-            }
-        }
-        private void CambiarlblSabor()
-        {
-            if (cmbTipoSabor.SelectedValue.ToString() == "1")
-            {
                 lblSabor.Text = "Sabor: Regular";
             }
-            else if (cmbTipoSabor.SelectedValue.ToString() == "2")
+            else if (cmbCodigo.SelectedValue.ToString() == "CO2")
             {
+                lblPrecio.Text = "Precio: $45";
+                lblMarca.Text = "Marca: Coca Cola";
                 lblSabor.Text = "Sabor: Zero";
             }
-            CambiarlblPrecio();
-        }
-        private void CambiarlblPrecio()
-        {
-            if (cmbTipoCodigo.SelectedValue.ToString() == "CO")
+            else if (cmbCodigo.SelectedValue.ToString() == "SP1")
             {
-                if (cmbTipoSabor.SelectedValue.ToString() == "1")
-                {
-                    lblPrecio.Text = "Precio: $35";
-                }
-                else if(cmbTipoSabor.SelectedValue.ToString() == "2")
-                {
-                    lblPrecio.Text = "Precio: $45";
-                }
-            }else if(cmbTipoCodigo.SelectedValue.ToString() == "SP")
-            {
-                if (cmbTipoSabor.SelectedValue.ToString() == "1")
-                {
-                    lblPrecio.Text = "Precio: $33";
-                }
-                else if (cmbTipoSabor.SelectedValue.ToString() == "2")
-                {
-                    lblPrecio.Text = "Precio: $43";
-                }
-            }else if(cmbTipoCodigo.SelectedValue.ToString() == "FA")
-            {
-                if (cmbTipoSabor.SelectedValue.ToString() == "1")
-                {
-                    lblPrecio.Text = "Precio: $50";
-                }
-                else if (cmbTipoSabor.SelectedValue.ToString() == "2")
-                {
-                    lblPrecio.Text = "Precio: $60";
-                }
+                lblPrecio.Text = "Precio: $33";
+                lblMarca.Text = "Marca: Sprite";
+                lblSabor.Text = "Sabor: Regular";
             }
+            else if (cmbCodigo.SelectedValue.ToString() == "SP2")
+            {
+                lblPrecio.Text = "Precio: $43";
+                lblMarca.Text = "Marca: Sprite";
+                lblSabor.Text = "Sabor: Zero";
+            }
+            else if (cmbCodigo.SelectedValue.ToString() == "FA1")
+            {
+                lblPrecio.Text = "Precio: $50";
+                lblMarca.Text = "Marca: Fanta";
+                lblSabor.Text = "Sabor: Regular";
+            }
+            else if (cmbCodigo.SelectedValue.ToString() == "FA2")
+            {
+                lblPrecio.Text = "Precio: $60";
+                lblMarca.Text = "Marca: Fanta";
+                lblSabor.Text = "Sabor: Zero";
+            }
+        }
+        private Boolean ValidarCampos()
+        {
+            bool valido = true;
+            string msg = string.Empty;
+            if(cmbCodigo.SelectedValue.ToString()=="(Elija una opción)")
+            {
+                msg = "Debe seleccionar un tipo\n";
+            }
+            if (msg != string.Empty)
+            {
+                valido = false;
+                MessageBox.Show(msg);
+            }
+            return valido;
         }
         #endregion
         private void frmIngresoLata_Load(object sender, EventArgs e)
         {
             CargarListaLatas(_expendedora.Latas);
-            CargarComboTipoCodigo();
-            CargarComboTipoSabor();
+            CargarComboCodigo();
         }
 
         private void btnApagar_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void cmbTipoCodigo_SelectedIndexChanged(object sender, EventArgs e)
         {
-            CambiarlblMarca();
+            Cambiarlbl();
         }
-
-        private void cmbTipoSabor_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            CambiarlblSabor();
-        }
-
         private void lblPrecio_Click(object sender, EventArgs e)
         {
 
+        }
+        private void btnVolver_Click(object sender, EventArgs e)
+        {
+            this.Owner.Show();
+            this.Dispose();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            if (txtVolumen.Text != String.Empty)
+            {
+                MessageBox.Show("Para agregar una nueva lata debe limpiar el formulario primero.");
+            }
+            else
+            {
+                try
+                {
+                    if (ValidarCampos())
+                    {
+                        Lata lataAIngresar = new Lata(cmbCodigo.SelectedValue.ToString(), txtVolumen.Text.ToñDo);
+                    }
+                }
+                catch(Exception e)
+                {
+
+                }
+            }
         }
     }
 }
